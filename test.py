23268bacf7cc866fd30ec5ff6861a026099c520f
virinarl <Cushita33984>
2020-07-05 22:58:46 -0300

    Prueba 1

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+import pandas as pd
+import requests
+
 def main():
-    texto = "Hola, bienvenidos al Repo del TP."
-    texto2 = "Pude agregar esta línea, Iri"
+    texto = "Probando."
     print(texto)
-    print(texto2)
 
 main()
