7da41595aa10650b6e0022152fca975737cab988
octavioalag <67447770+octavioalag@users.noreply.github.com>
2020-07-07 12:44:21 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
-def main():
-    texto = "Hola, bienvenidos al Repo del TP."
-    texto2 = "Pude agregar esta línea, Iri"
-    print(texto)
-    print(texto2)
+from PIL import Image
 
+def ingreso_ruta(pixeles):
+    while pixeles == []:
+        ruta = input("Nombre de la imagen:")
+        pixeles = contadorPixel(ruta,pixeles)
+
+
+def contador_pixel(ruta,pixeles):
+
+    try:
+        im = Image.open(ruta)
+        for pixel in im.getdata():
+            if pixel == (255,0,0):
+                rojo += 1
+            elif pixel == (0,255,0):
+                    verde += 1
+            elif pixel ==(0,0,255):
+                azul += 1
+        pixeles = [rojo,verde,azul]
+    
+    except:
+        print("No ha sido posible cargar la imagen, de nombre: "+ruta)
+    
+    return pixeles
+
+def main():
+    pixeles=[]
+    ingreso_ruta(pixeles)
 main()
