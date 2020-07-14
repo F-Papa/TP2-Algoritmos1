a558f7e02d66324497632f4f5f671e677f418639
F-Papa <67394591+F-Papa@users.noreply.github.com>
2020-07-14 00:47:31 -0700

    CSV 1.1.0

--- CSV/modulo_csv.py
+++ CSV/modulo_csv.py
@@ -1,30 +1,12 @@
 import csv
 import datetime
-import modulo_graficos as graf
 
-path_archivo = 'C:\\Users\\franc\\source\\repos\\SMN TP\\weatherdata--389-603.csv'   #Aca va el directorio del archivo
+path_archivo = 'C:\\Users\\franc\\source\\repos\\SMN TP\\weatherdata--389-603.csv'   # (Solo para pruebas internas)
 
-def lista_a_int(lista):
-    """Pre-Condicion: Recibe un numero como lista de digitos
-    Post-Condicion: Devuelve ese numero como un int"""
-    
-    num_a_devolver = 0
-    for i in range(len(lista)):
-        num_a_devolver += int(lista[-1-i])*(10**i)
-    return num_a_devolver
-
-def extraer_año(linea):
-    """Pre-Condicion: Recibe una lista en la que el primer item es un año (cadena)
-    Post-Condicion: Devuelve el año de esa entrada (int)"""
-
-    buffer=[]
-    for i in range(4):
-        buffer.append(linea[0][-4+i])
-    return lista_a_int(buffer)
-
-def get_max(archivo, posicion_valor, numero_años = 1):
-    """Pre-Condicion Recibe los valores de un archivo csv (cadena), la posicion del
-    valor cuyo maximo se desea averiguar (int), y la cantidad de años que se desea tener cuenta (int, 1 por defecto)
+def get_max(archivo, posicion_valor, años_a_retroceder = 0):
+    """ Pre-Condicion Recibe los valores de un archivo csv (cadena), la posicion del
+        valor cuyo maximo se desea averiguar (int), y la cantidad de años que se desea tener cuenta (int, 0 por defecto). Si años_a_retroceder es igual a 0, solo se
+        tendran en cuenta los datos del año actual.
         Post-Condicion: Devuelve el maximo de esos valores"""
 
     año_actual = get_año_actual()
@@ -35,11 +17,29 @@ def get_max(archivo, posicion_valor, numero_años = 1):
     for linea in valores:
         año_linea = extraer_año(linea)
 
-        if numero_años > (año_actual - año_linea):
+        if años_a_retroceder > (año_actual - año_linea):
             if max < float(linea[posicion_valor]):
                 max = float(linea[posicion_valor])
     
     return max
+
+def lista_a_int(lista):
+    """ Pre-Condicion: Recibe un numero como lista de digitos
+        Post-Condicion: Devuelve ese numero como un int"""
+    
+    num_a_devolver = 0
+    for i in range(len(lista)):
+        num_a_devolver += int(lista[-1-i])*(10**i)
+    return num_a_devolver
+
+def extraer_año(linea):
+    """ Pre-Condicion: Recibe una lista en la que el primer item es un año (cadena)
+        Post-Condicion: Devuelve el año de esa entrada (int)"""
+
+    buffer=[]
+    for i in range(4):
+        buffer.append(linea[0][-4+i])
+    return lista_a_int(buffer)
 
 def get_año_actual():
     """Post-Condicion: devuelve el año actual (int)"""
@@ -47,29 +47,42 @@ def get_año_actual():
     fecha_actual = datetime.datetime.now()
     return fecha_actual.year
 
-def promedio_reciente(archivo, posicion_valor, numero_años = 1):
-    """Pre-Condicion Recibe los valores de un archivo csv (cedena), la posicion del
-    valor que se desea promediar (int), y la cantidad de años que se desea tener cuenta (int, 1 por defecto)
-        Post-Condicion: Devuelve un promedio de todos esos valores"""
-    
+def get_promedio(archivo, posicion_valor, años_a_retroceder=0):
+    """ Pre-Condicion: Recibe una lista de lineas en formato CSV (archivo), la posicion en la lista del valor que se desea promediar (posicion_valor, int)
+        y la cantidad de años que se desean revisar (años_a_retroceder, int, 0 por defecto). Si años_a_retroceder es igual a 0, solo se promediaran los datos del año actual.
+        Post-Condicion: devuelve una lista con los promedios del valor ubicado en la posicion pedida para cada año."""
+
     valores = csv.reader(archivo)
     next(valores)
-    año_actual = get_año_actual()
+    lineas = [linea for linea in valores]
+    
     sumatoria = float(0)
-    contador = 0
+    contador_items = float(0)
+    lista_a_devolver = []
+    
+    año_actual = get_año_actual()
+    contador_años = 0
+    i = len(lineas)-1
 
-    for linea in valores:
-        año_linea = extraer_año(linea)
+    while i>=0 and contador_años < años_a_retroceder:
+        
+        año_linea = extraer_año(lineas[i])
+        año_linea_sig = extraer_año(lineas[i-1])
 
-        if numero_años > (año_actual - año_linea):
-            sumatoria += float(linea[posicion_valor])
-            contador += 1
+        if (año_actual - año_linea) < años_a_retroceder:
 
-    if contador != 0:
-        return sumatoria/contador
+            sumatoria += float(lineas[i][posicion_valor])
 
-    else: 
-        return 0
+            contador_items += 1
+
+            if i == 0 or año_linea_sig != año_linea:
+                lista_a_devolver.append(sumatoria/contador_items)
+                contador_años +=1
+                contador_items, sumatoria = 0, 0
+            
+        i -=1
+
+    return lista_a_devolver
 
 def cargar_archivo(directorio):
     """ Pre-Condicion: Recibe el directorio de un archivo csv como cadena.
@@ -81,24 +94,14 @@ def cargar_archivo(directorio):
     return lista_a_devolver    
 
 def main():
-    """Procedimiento para probar el correcto funcionamiento de las otras funciones"""
-
+    """ Procedimiento para probar el correcto funcionamiento de las otras funciones"""
+    
     lineas_archivo = cargar_archivo(path_archivo)
 
-    n_años = 8
-
-    promedio_temp_max = promedio_reciente(lineas_archivo, 4, n_años)
-    promedio_temp_min = promedio_reciente(lineas_archivo, 5, n_años)
-    promedio_temp_total = (promedio_temp_max + promedio_temp_min)/2
+    print(get_promedio(lineas_archivo, 8, 8))
+    print(get_max(lineas_archivo, 4, 8))
+    print(get_max(lineas_archivo, 8, 8))
     
-    print("Temperatura promedio de los ultimos {} años: {}.".format(n_años, promedio_temp_total))
-    
-    promedio_humedad = promedio_reciente(lineas_archivo, 8, n_años)
-    print("Humedad promedio de los ultimos {} años: {}.".format(n_años, promedio_humedad))
-
-    print(get_max(lineas_archivo, 4, n_años))
-    print(get_max(lineas_archivo, 8, n_años))
-    
-main()
+#main() # (Solo para pruebas internas)
 
 
