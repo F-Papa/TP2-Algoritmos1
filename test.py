a06ce2c7eb6c5368015f1dbf7f23daaa920eb7ee
virinarl <Cushita33984>
2020-07-15 22:58:02 -0300

    Función para extraer la información del pronóstico extendido

--- test.py
+++ test.py
@@ -10,6 +10,27 @@ def smn_request(diccionario):
         nombre = key+'.txt'
         with open(nombre, 'wb') as archivo:
             archivo.write(resp.content)
+
+def extendido(nombre_archivo):
+    """Generá una lista con los datos necesarios de cada una de las ciudades incluidas en los archivos relacionados con el
+    pronóstico extendido. 
+    Precondición: El nombre del archivo que se desea procesar"""
+    
+    info = []
+    with open(nombre_archivo + ".txt","r", encoding="utf-8") as archivo:
+        data = archivo.readlines()
+        for elemento in data:
+            elemento_json = json.loads(elemento)
+            for i in range(len(elemento_json)):
+                info.append({"Ciudad": elemento_json[i]["name"],
+                             "Provincia": elemento_json[i]["province"],
+                             "Latitud": elemento_json[i]["lat"],
+                             "Longitud": elemento_json[i]["lon"],
+                             "Temperatura_mañana": elemento_json[i]["weather"]["morning_temp"],
+                             "Descripción_mañana": elemento_json[i]["weather"]["morning_desc"],
+                             "Temperatura_tarde": elemento_json[i]["weather"]["afternoon_temp"],
+                             "Descripción_tarde": elemento_json[i]["weather"]["afternoon_desc"]})
+    return info
         
 
 def main():
