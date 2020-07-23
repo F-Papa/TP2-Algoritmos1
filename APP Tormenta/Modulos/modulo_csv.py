d7873d80f1e70466789100d0a8e2adfeab450abb
F-Papa <67394591+F-Papa@users.noreply.github.com>
2020-07-23 13:53:52 -0700

    Agrego los modulos CSV Geo y Graficos

---
+++ APP Tormenta/Modulos/modulo_csv.py
@@ -0,0 +1,95 @@
+import csv
+import datetime
+
+def get_max(archivo, posicion_valor, años_a_retroceder = 0):
+    """ Pre-Condicion Recibe los valores de un archivo csv (cadena), la posicion del
+        valor cuyo maximo se desea averiguar (int), y la cantidad de años que se desea tener cuenta (int, 0 por defecto). Si años_a_retroceder es igual a 0, solo se
+        tendran en cuenta los datos del año actual.
+        Post-Condicion: Devuelve el maximo de esos valores"""
+
+    año_actual = get_año_actual()
+    valores = csv.reader(archivo)
+    next(valores)
+    max = float(0)
+
+    for linea in valores:
+        año_linea = extraer_año(linea)
+
+        if años_a_retroceder > (año_actual - año_linea):
+            if max < float(linea[posicion_valor]):
+                max = float(linea[posicion_valor])
+    
+    return max
+
+def lista_a_int(lista):
+    """ Pre-Condicion: Recibe un numero como lista de digitos
+        Post-Condicion: Devuelve ese numero como un int"""
+    
+    num_a_devolver = 0
+    for i in range(len(lista)):
+        num_a_devolver += int(lista[-1-i])*(10**i)
+    return num_a_devolver
+
+def extraer_año(linea):
+    """ Pre-Condicion: Recibe una lista en la que el primer item es un año (cadena)
+        Post-Condicion: Devuelve el año de esa entrada (int)"""
+
+    buffer=[]
+    for i in range(4):
+        buffer.append(linea[0][-4+i])
+    return lista_a_int(buffer)
+
+def get_año_actual():
+    """Post-Condicion: devuelve el año actual (int)"""
+
+    fecha_actual = datetime.datetime.now()
+    return fecha_actual.year
+
+def get_promedio(archivo, posicion_valor, años_a_retroceder=0):
+    """ Pre-Condicion: Recibe una lista de lineas en formato CSV (archivo), la posicion en la lista del valor que se desea promediar (posicion_valor, int)
+        y la cantidad de años que se desean revisar (años_a_retroceder, int, 0 por defecto). Si años_a_retroceder es igual a 0, solo se promediaran los datos del año actual.
+        Post-Condicion: devuelve una lista con los promedios del valor ubicado en la posicion pedida para cada año."""
+
+    valores = csv.reader(archivo)
+    next(valores)
+    lineas = [linea for linea in valores]
+    
+    sumatoria = float(0)
+    contador_items = float(0)
+    lista_a_devolver = []
+    
+    año_actual = get_año_actual()
+    contador_años = 0
+    i = len(lineas)-1
+
+    while i>=0 and contador_años < años_a_retroceder:
+        
+        año_linea = extraer_año(lineas[i])
+        año_linea_sig = extraer_año(lineas[i-1])
+
+        if (año_actual - año_linea) < años_a_retroceder:
+
+            sumatoria += float(lineas[i][posicion_valor])
+
+            contador_items += 1
+
+            if i == 0 or año_linea_sig != año_linea:
+                lista_a_devolver.append(sumatoria/contador_items)
+                contador_años +=1
+                contador_items, sumatoria = 0, 0
+            
+        i -=1
+
+    return lista_a_devolver
+
+def cargar_archivo(directorio):
+    """ Pre-Condicion: Recibe el directorio de un archivo csv como cadena.
+        Pos-Condicion: Devuelve una lista en la que cada linea (separada por endline) es una item"""
+
+    with open(directorio, newline='\n') as archivo:
+        lista_a_devolver = [linea for linea in archivo]
+
+    return lista_a_devolver    
+
+
+
---
+++ APP Tormenta/Modulos/modulo_graficos.py
@@ -0,0 +1,73 @@
+class Color:                     #Se crea una clase para poder acceder a sus miembros desde afuera del modulo en caso de ser necesario.
+    ROJO   = "\033[1;31m"  
+    AZUL  = "\033[1;34m"
+    CYAN  = "\033[1;36m"
+    VERDE = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD    = "\033[;1m"
+    REVERSE = "\033[;7m"
+    ARCOIRIS = "ARCOIRIS"
+
+
+colores = [Color.ROJO, Color.AZUL, Color.CYAN, Color.VERDE, Color.RESET]  #Se elije usar una lista para poder recorrerla con un indice, cosa que el diccionario no permite.
+
+def get_max_min(valores):
+    """ Pre-Condicion: Recibe una iterable con elementos numericos
+        Post-Condicion: Devuelve una tupla del modo (valor_maximo, valor_minimo)"""
+
+    max, min = float(0), float(0)
+    contador = 0
+    
+    for item in valores:
+    
+        if contador == 0:
+            max, min = item, item
+
+        if item>max:
+            max = item
+        elif item<min:
+            min = item
+
+        contador+=1
+    
+    return (max, min)
+
+def grafico_barras(eje_vertical, eje_horizontal, unidad_medicion = "", flag_color = Color.RESET):
+    """Pre-Condicion: Recibe a las listas eje_vertical (cualquier tipo) y eje_horizontal (numerico). El usuario tiene la opcion
+    de ingresar un sufijo/unidad (unidad_medicion) y una bandera de color (Color.ROJO, Color.AZUL, Color.CYAN, Color.VERDE, Color.ARCOIRIS, Color.RESET).
+    Post-Condicion: Imprime un grafico de barras con los datos ingresados en el color deseado. Si se ingreso una unidad de medicion, esta aparece despues de cada valor.
+    Si los valores ingresados son compatibles entre si devuelve 1, en caso contrario devuelve 0 y no se imprime ningun gráfico"""    
+
+    if len(eje_horizontal) != len(eje_vertical):    #No se puede graficar pues el eje vertical y el eje x no tienen la misma cantidad de elementos
+        return 0
+    
+    max_min = get_max_min(eje_horizontal)
+    rango = max_min[0] - max_min[1]
+    escala = (rango)/100
+    i = 0
+
+    for j in range(len(eje_horizontal)):
+
+        #Interpretacion flag color
+        if flag_color == "ARCOIRIS":
+            if i < len(colores)-1:
+                i+=1
+            else:
+                i=0     
+        else:
+            i = colores.index(flag_color)  
+
+        #Activa el color        
+        print(colores[i], end= "") 
+
+        #Imprime el grafico
+        if eje_horizontal[j] == 0:
+           print(eje_vertical[j], " ", eje_horizontal[j], unidad_medicion)
+           
+        else:
+            unidades = round((eje_horizontal[j]-max_min[1])/escala)
+            print(eje_vertical[j], " ", "█"*(unidades+3), eje_horizontal[j],unidad_medicion)
+
+    #Resetea el color a blanco   
+    print (Color.RESET)   
+    return 1
